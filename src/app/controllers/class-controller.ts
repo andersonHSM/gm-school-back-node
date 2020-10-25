441fcc4b7b2dde54910467a54157d66dcf698538
Anderson Henrique <andersonmenezes.dev@gmail.com>
2020-10-25 08:14:20 -0300

    listagem de classes

--- src/app/controllers/class-controller.ts
+++ src/app/controllers/class-controller.ts
@@ -1,14 +1,25 @@
 import { HttpException } from '@exceptions/index';
 import { BaseController } from '@models/index';
+import { AuthenticatedRequest } from '@models/requests/auth';
 import { ClassInsertPayload } from '@models/requests/class';
 import { ClassService } from '@services/index';
 import { Request, Response } from 'express';
 
 export class ClassController implements BaseController {
   constructor(private readonly classService: ClassService) {}
-  index() {
-    throw new Error('Method not implemented.');
-  }
+  index = async (_req: Request & AuthenticatedRequest, res: Response) => {
+    try {
+      const classes = await this.classService.getAllClasses();
+
+      return res.status(200).json(classes);
+    } catch (error) {
+      if (error instanceof HttpException) {
+        return res.status(error.statusCode).json(error.format());
+      }
+
+      return res.status(500).json({ error: error.message });
+    }
+  };
 
   store = async (req: Request<null, null, ClassInsertPayload>, res: Response) => {
     const payload = req.body;
--- src/app/database/accessors/class.ts
+++ src/app/database/accessors/class.ts
@@ -6,6 +6,21 @@ import { v4 as uuidv4, parse as uuidParse, stringify as uuidStringfy } from 'uui
 export class Class {
   constructor(private readonly knex: Knex) {}
 
+  getAllClasses = async (returningFields: string[]) => {
+    const classes: ClassModel[] = (
+      await this.knex('class').select([...returningFields, 'class_guid'])
+    ).map((classReturn: ClassModel) => {
+      const { class_guid, class_stage_guid } = classReturn;
+      return {
+        ...classReturn,
+        class_guid: uuidStringfy(class_guid as ArrayLike<number>),
+        class_stage_guid: uuidStringfy(class_stage_guid as ArrayLike<number>),
+      };
+    });
+
+    return classes;
+  };
+
   insertClass = async (payload: ClassInsertPayload, returningFields: string[]) => {
     const class_stage_guid = this.verifyUuid(payload.class_stage_guid);
     const class_guid = this.verifyUuid(uuidv4());

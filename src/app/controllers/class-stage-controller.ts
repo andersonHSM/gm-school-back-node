f007363b950614f4158d005a41e72ba154577374
Anderson Menezes <andersonmenezes.dev@gmail.com>
2020-10-27 18:03:15 -0300

    ajuste

--- src/app/controllers/class-stage-controller.ts
+++ src/app/controllers/class-stage-controller.ts
@@ -1,5 +1,7 @@
 import { ClassStage } from '@database/accessors';
+import { HttpException } from '@exceptions/index';
 import { BaseController } from '@models/index';
+import { Request, Response } from 'express';
 
 export class ClassStageController implements BaseController {
   constructor(private readonly classStage: ClassStage) {}
@@ -7,9 +9,16 @@ export class ClassStageController implements BaseController {
   index() {
     throw new Error('Method not implemented.');
   }
-  store() {
-    throw new Error('Method not implemented.');
-  }
+  store = (req: Request, res: Response) => {
+    try {
+    } catch (error) {
+      if (error instanceof HttpException) {
+        return res.status(error.statusCode).json(error.format());
+      }
+
+      return res.status(500).json(error.message);
+    }
+  };
   delete() {
     throw new Error('Method not implemented.');
   }
